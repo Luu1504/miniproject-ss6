640e1a7b9e170c886e97cf49b54dc6ab0b356879
luu1504 <hotsuper75@gmail.com>
2026-02-06 09:59:54 +0700

    lession6

--- index.js
+++ index.js
@@ -10,108 +10,79 @@ while (loginAttempts < 3) {
         isAuthenticated = true;
         break;
     } else {
-        loginAttempts++;
+        loginAttempts = loginAttempts + 1;
         if (loginAttempts < 3) {
-            alert("Wrong credentials. You have " + (3 - loginAttempts) + " attempts left.");
+            alert("Wrong! Attempts left: " + (3 - loginAttempts));
         }
     }
 }
 
-if (!isAuthenticated) {
+if (isAuthenticated === false) {
     alert("Tài khoản đã bị khóa");
 } else {
     let isRunning = true;
-
-    while (isRunning) {
-        let menu = "--- Library Menu ---\n";
-        menu += "1. Add new books\n";
-        menu += "2. Show all books\n";
-        menu += "3. Search book\n";
-        menu += "4. Update book\n";
-        menu += "5. Reverse books\n";
-        menu += "6. Merge from other source\n";
-        menu += "7. Exit\n";
-        menu += "Select an option:";
-
+    while (isRunning === true) {
+        let menu = "1. Add books\n2. Show books\n3. Search\n4. Update\n5. Reverse\n6. Merge\n7. Exit";
         let choice = prompt(menu);
 
-        switch (choice) {
-            case "1":
-                let inputBooks = prompt("Enter book names (separated by comma):");
-                let newBooksArray = inputBooks.split(",");
-                for (let i = 0; i < newBooksArray.length; i++) {
-                    libraries.push(newBooksArray[i].trim());
+        if (choice === "1") {
+            let newBook = prompt("Enter book name:");
+            libraries[libraries.length] = newBook;
+            alert("Added 1 book.");
+        }
+        else if (choice === "2") {
+            let list = "";
+            for (let i = 0; i < libraries.length; i++) {
+                list = list + (i + 1) + ". " + libraries[i] + "\n";
+            }
+            console.log(list);
+        }
+        else if (choice === "3") {
+            let searchName = prompt("Book name:");
+            let found = false;
+            for (let i = 0; i < libraries.length; i++) {
+                if (libraries[i] === searchName) {
+                    alert("Found at index: " + i);
+                    found = true;
+                    break;
                 }
-                alert("Added " + newBooksArray.length + " books to library.");
-                break;
-
-            case "2":
-                let displayString = "";
-                for (let i = 0; i < libraries.length; i++) {
-                    displayString += (i + 1) + ". " + libraries[i] + "\n";
+            }
+            if (found === false) alert("Not found");
+        }
+        else if (choice === "4") {
+            let oldName = prompt("Old name:");
+            let foundIndex = -1;
+            for (let i = 0; i < libraries.length; i++) {
+                if (libraries[i] === oldName) {
+                    foundIndex = i;
+                    break;
                 }
-                console.log(displayString);
-                alert("Book list printed in Console.");
-                break;
-
-            case "3":
-                let searchName = prompt("Enter book name to search:");
-                let foundIndex = -1;
-                for (let i = 0; i < libraries.length; i++) {
-                    if (libraries[i] === searchName) {
-                        foundIndex = i;
-                        break;
-                    }
-                }
-                if (foundIndex !== -1) {
-                    alert("Sách " + searchName + " được tìm thấy tại vị trí " + foundIndex);
-                } else {
-                    alert("Không tìm thấy");
-                }
-                break;
-
-            case "4":
-                let oldName = prompt("Enter book name to update:");
-                let updateIndex = -1;
-                for (let i = 0; i < libraries.length; i++) {
-                    if (libraries[i] === oldName) {
-                        updateIndex = i;
-                        break;
-                    }
-                }
-                if (updateIndex !== -1) {
-                    let newName = prompt("Enter new name:");
-                    libraries[updateIndex] = newName;
-                    alert("Update successful.");
-                } else {
-                    alert("Error: Book does not exist.");
-                }
-                break;
-
-            case "5":
-                libraries.reverse();
-                let reverseString = "";
-                for (let i = 0; i < libraries.length; i++) {
-                    reverseString += "Index " + i + ": " + libraries[i] + "\n";
-                }
-                console.log(reverseString);
-                alert("Reversed order printed in Console.");
-                break;
-
-            case "6":
-                let otherSource = ["Sách Kỹ Năng", "Truyện Tranh"];
-                libraries = libraries.concat(otherSource);
-                alert("Đã gộp kho sách thành công");
-                break;
-
-            case "7":
-                alert("Hẹn gặp lại!");
-                isRunning = false;
-                break;
-
-            default:
-                alert("Invalid choice. Please try again.");
-                break;
+            }
+            if (foundIndex !== -1) {
+                let newName = prompt("New name:");
+                libraries[foundIndex] = newName;
+                alert("Success!");
+            } else {
+                alert("Not found");
+            }
+        }
+        else if (choice === "5") {
+            let reversedList = "";
+            for (let i = libraries.length - 1; i >= 0; i--) {
+                reversedList = reversedList + "Index " + i + ": " + libraries[i] + "\n";
+            }
+            console.log(reversedList);
+        }
+        else if (choice === "6") {
+            let otherSource = ["Sách Kỹ Năng", "Truyện Tranh"];
+            for (let i = 0; i < otherSource.length; i++) {
+                libraries[libraries.length] = otherSource[i];
+            }
+            alert("Merged successfully");
+        }
+        else if (choice === "7") {
+            alert("Hẹn gặp lại!");
+            isRunning = false;
         }
     }
 }
